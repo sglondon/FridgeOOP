df86ec304909becbbc2ea7310381502930cec8df
sglondon <susangs2@gmail.com>
2018-01-31 14:08:02 -0500

    initial commit

---
+++ FridgeOOP/Fridge.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FridgeOOP
+{
+    public class Fridge
+    {
+        //fields
+
+        private bool isClean;
+        private string dispenserType;
+        private int foodAmount;
+        private bool bulbsWork;
+
+        //Properties
+
+        public bool IsClean
+        {
+            get { return this.isClean; }
+            set { this.isClean = value; }
+        }
+
+        public string DispenserType
+        {
+            get { return this.dispenserType; }
+        }
+
+        public int FoodAmount
+        {
+            get { return this.foodAmount; }
+            set { this.foodAmount = value; }
+        }
+
+        public bool BulbsWork
+        {
+            get { return this.bulbsWork; }
+            set { this.bulbsWork = value; }
+        }
+
+        //Constructors
+
+        //Methods()
+
+
+    }
+}
---
+++ FridgeOOP/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace FridgeOOP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
